e4d412eea63027c51f9270634b9f39ef184661c0
Tran Thai Duong <tranthaiduong95969807@gmail.com>
2025-04-26 11:34:12 +0700

    feat(api): Added 'YoloService' class for serving API

--- service/object_detection/yolo_demo.py
+++ service/object_detection/yolo_demo.py
@@ -28,6 +28,7 @@ class_names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'tra
                'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 
                'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 
                'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
 '''
     Perform non maximum suppresion for multiple bounding boxes detected of many overlapping predictions.
 
